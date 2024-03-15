be1c9398c200aa968830ab4067fbd5889d96bbfc
hoangthanhtung2001 <99401621+hoangthanhtung2001@users.noreply.github.com>
2024-03-15 16:02:43 +0900

    Update mouse_setting.py

--- mouse_setting.py
+++ mouse_setting.py
@@ -3,6 +3,7 @@ import time
 from screen import get_location_object
 import os 
 import glob
+from wait import check_image_on_screen_by_around_image
 # Di chuyển chuột đến tọa độ (x, y)
 def find_latest_png_in_directory(directory):
     # Tạo đường dẫn đầy đủ đến thư mục
@@ -20,21 +21,58 @@ def find_latest_png_in_directory(directory):
     # Trả về đường dẫn của file PNG mới nhất
     return png_files[0]
 
+def mouse_movement_around_Image(x,y):
+    pyautogui.moveTo(x, y)
+
+def mouse_click_by_around_Image(x,y):
+    pyautogui.click(x=x,y=y)
+
+def right_click_around_Image(x,y):
+    pyautogui.moveTo(x, y)
+    pyautogui.click(button="right")
+
+def double_click_around_Image(x,y):
+    pyautogui.doubleClick(x,y)
+
+def three_click_around_Image(x,y):
+    pyautogui.tripleClick(x,y)
+
+def click_and_drag_around_Image(x,y,x2,y2):
+    pyautogui.moveTo(x, y)
+    pyautogui.mouseDown(button="left",x=x,y=y)
+    pyautogui.moveTo(x2, y2)
+    pyautogui.mouseUp(button='left',x=x2,y=y2)
+
+def mouse_scroll_up_around_Image(count,x,y):
+    if count>0:
+            pyautogui.moveTo(x, y)
+            pyautogui.scroll(count,x=x,y=y)
+    else:
+            pyautogui.scroll(0)
+            
+def mouse_scroll_down_around_Image(count,x,y):
+    if count<0:
+            pyautogui.moveTo(x, y)
+            pyautogui.scroll(count,x=x,y=y)
+    else:
+            pyautogui.scroll(0)
+            
+
 def mouse_movement(image_path):
     if get_location_object(image_path) != 0:
         x,y=get_location_object(image_path)
         pyautogui.moveTo(x, y)
     else:
         print("Hình ảnh không tồn tại trên màn hình của cửa sổ hiện tại.")
-   
+
+
 def mouse_click(image_path):
     if get_location_object(image_path) != 0:
         x,y =get_location_object(image_path)
         pyautogui.click(x=x,y=y)
     else:
         print("Hình ảnh không tồn tại trên màn hình của cửa sổ hiện tại.")
-    
-    
+
 def right_click(image_path):
     if get_location_object(image_path) != 0:
         x,y =get_location_object(image_path)
